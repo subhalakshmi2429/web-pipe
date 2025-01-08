9f5bcb09b5aa67f8fd83007b10307f496ccf71b7
subhalakshmi2429 <subhalakshmi2429@gmail.com>
2025-01-08 16:00:11 +0530

    boto3 commit

--- botos3.py
+++ botos3.py
@@ -2,64 +2,61 @@ import boto3
 import os
 import requests
 
-# Get the AWS credentials and region from environment variables
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID','Default Value')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY','Default Value')
-AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION','Default Value')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', 'Default Value')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', 'Default Value')
+AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION', 'Default Value')
 
 # Initialize the S3 client
-client = boto3.client('s3', 
-                      aws_access_key_id=AWS_ACCESS_KEY_ID,
-                      aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                      region_name=AWS_DEFAULT_REGION)
+client = boto3.client('s3')
 
 try:
-    bucket_name = "jenkin_bucket"
-    
+    bucket_name = "contain-buck"
     # Create the S3 bucket
     response = client.create_bucket(
         Bucket=bucket_name,
+        #ObjectLockEnabledForBucket=False,
         ObjectOwnership='BucketOwnerPreferred'
     )
-    
-    # Unblock public access
+    #puclic access unblock panna
     response = client.delete_public_access_block(
         Bucket=bucket_name,
         ExpectedBucketOwner='890742598563'
     )
 
+    #response = client.get_bucket_ownership_controls(
+     #    Bucket=bucket_name,
+         #ExpectedBucketOwner='026090548174'
+    #)
     print("Bucket created successfully:", response)
-except Exception as e:
-    print(f"Error creating bucket: {e}")
+except :
+    print("Error creating bucket:")
 
-# Fetch webpage content
-url = os.getenv('URL', 'https://dev.to/')  # Default URL in case URL is not set
+
+# webpage source code
+#url = "https://dev.to/" 
+url = os.getenv('URL', 'Default Value')
 print(f"Enter the URL: {url}")
 response = requests.get(url)
-
-try:
-    response.raise_for_status()  # Will raise an exception for non-200 responses
-    if response.status_code == 200:
-        webpage_content = response.text
-    else:
-        print("Failed to retrieve the webpage")
-        exit()
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching webpage: {e}")
+response.raise_for_status() 
+if response.status_code == 200:
+    webpage_content = response.text
+else:
+    print("Failed to retrieve the webpage")
     exit()
 
-# Save the content with UTF-8 encoding
+#  Save the content with UTF-8 encoding
 file_name = "source.html"
 with open(file_name, "w", encoding="utf-8") as file:
     file.write(webpage_content)
 
+
 # Upload the file to S3
-s3 = boto3.client('s3', 
-                  aws_access_key_id=AWS_ACCESS_KEY_ID,
-                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                  region_name=AWS_DEFAULT_REGION)
+s3 = boto3.client('s3')
+bucket_name = bucket_name  
+
 try:
+    #s3.upload_file(file_name, bucket_name, file_name)
     s3.upload_file(file_name, bucket_name, file_name, ExtraArgs={'ACL': 'public-read', 'ContentType': 'text/html'})
     print(f"File {file_name} uploaded to S3 successfully!")
-except Exception as e:
-    print(f"Error uploading file to S3: {e}")
+except:
+    print(f"Error uploading file to S3")
